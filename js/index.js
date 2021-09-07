2af5b0ba5d78a66f5d983155c519a84fb24da038
Kozimjon7688 <kozimjonomonjonov88@gmail.com>
2021-09-07 03:53:50 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var intro = alert("Xush kelibsiz bizning saytga!");
+var surName = prompt("Familyangizni kiriting!", "").trim();
+var firstName = prompt("Ismingizni kiriting!", "").trim();
+var age = prompt("Yoshingizni kiriting!", "").trim();
+var telNumber = prompt("Telefon raqamingizni kiriting!", "").trim();
+var reEntry = alert("Ro'yhatdan muvofaqiyatli o'tdingiz!.Robot emasligingizni tasdiqlash uchun yuqoridagi ro'yhatni yana to'ldiring!")
+
+
+console.log(
+  `Familyangiz: ${surName}\nIsmingiz: ${firstName}\nYoshingiz: ${age}\nTelefon raqamingiz: ${telNumber}`
+);
+
+
+
+
+// concatinatsiya
